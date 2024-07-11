62854c596b5f011e9cfb5764e6cdfb11f348a5de
Ashwinii2024 <pawar.ashwini3004@gmail.com>
2024-07-11 15:27:15 +0530

    array_function

---
+++ Array_Fuctions/BooleanArray.java
@@ -0,0 +1,23 @@
+package Array_Fuctions;
+
+import java.util.Arrays;
+
+public class BooleanArray 
+
+{
+
+	public static void main(String[] args) 
+	
+	{
+	 
+		boolean b[] = new boolean[2];
+		b[0] = true;
+		b[1] = false;
+	//	int age =24;
+		
+			System.out.println(Arrays.toString(b));
+	
+		
+	}
+
+}
---
+++ Array_Fuctions/CharArray.java
@@ -0,0 +1,18 @@
+package Array_Fuctions;
+
+import java.util.Arrays;
+
+public class CharArray {
+
+	public static void main(String[] args) 
+	
+	{
+		char c[] = new char[2];
+		c[0] ='a';
+		c[1] ='b';
+		
+	System.out.println(Arrays.toString(c));
+
+	}
+
+}
