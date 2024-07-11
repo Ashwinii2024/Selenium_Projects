14db1a184ceac54556f588fa2ae5d7985d0e100f
Ashwinii2024 <pawar.ashwini3004@gmail.com>
2024-07-11 14:47:34 +0530

    conditional statement

---
+++ conditional_Statement/If_Statement.java
@@ -0,0 +1,15 @@
+package conditional_Statement;
+
+public class If_Statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int age = 23;
+    if(age < 21) {
+    	System.out.println("Age is less than 21");
+    }
+    
+    System.out.println("Age is greate than 21");
+	}
+
+}
