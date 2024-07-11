190ebedaf783f665852a288d46eab0f6f7987913
Ashwinii2024 <pawar.ashwini3004@gmail.com>
2024-07-11 14:05:13 +0530

    accessspecifier

---
+++ Access_Specifier_Within_Package/class1.java
@@ -0,0 +1,27 @@
+package Access_Specifier_Within_Package;
+
+public class class1
+
+{
+   void add() 
+	{
+	System.out.println("1");	
+	
+	}
+	
+    protected static void sub() 
+    {
+	 System.out.println("2");	
+	}
+
+    private static void mul() 
+    {
+	 System.out.println("3");
+}
+
+     static void div() 
+     {
+	 System.out.println("4");
+
+}
+}
