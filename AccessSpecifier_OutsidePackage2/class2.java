b98f24072b2f9791c0f0b04c961ffd117f7a5541
Ashwinii2024 <pawar.ashwini3004@gmail.com>
2024-07-11 14:39:40 +0530

    accesss[ecifieroutsidepkg2

---
+++ AccessSpecifier_OutsidePackage2/class2.java
@@ -0,0 +1,18 @@
+package AccessSpecifier_OutsidePackage2;
+
+import AccessSpecifier_OutsidePackage1.Class1;
+
+public class class2 
+
+{
+	
+	public static void main(String[] args)
+	
+			{
+		Class1 c1 = new Class1();
+		
+		c1.add() ;
+		
+	        }
+
+}
