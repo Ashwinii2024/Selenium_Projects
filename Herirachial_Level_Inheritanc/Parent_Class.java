12ee609438a163c2c2bf03c0cfec80a9ccd88ccc
Ashwinii2024 <pawar.ashwini3004@gmail.com>
2024-07-11 14:49:24 +0530

    herirachical

---
+++ Herirachial_Level_Inheritanc/Parent_Class.java
@@ -0,0 +1,13 @@
+package Herirachial_Level_Inheritanc;
+
+public class Parent_Class 
+
+{
+	
+	Parent_Class() {
+		
+		System.out.println("Parent_Class");
+	}
+			
+
+}
---
+++ Herirachial_Level_Inheritanc/class4.java
@@ -0,0 +1,16 @@
+package Herirachial_Level_Inheritanc;
+
+public class class4 extends Parent_Class
+
+{ 
+	void pappa() {
+		System.out.println("Pappa");
+	}
+	
+	public static void main(String[] args) {
+		
+		class4 c4 = new class4();
+		c4.pappa();
+	}
+
+}
